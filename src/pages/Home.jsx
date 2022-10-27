87338d1b2cfcc41834c744fa52988e0d74824ba2
Eno <eno.meta@gmx.fr>
2022-10-27 14:00:40 +0200

    React Crash Course finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,21 +12,31 @@ function Home() {
         console.log(data)
     }
     useEffect(() => {
+        setTimeout(() => {
             fetchUsers();
+        }, 2000);
     }, []);
+
+    function renderUsers() {
+        return users.map((user) => (
+            <Link to={`/users/${user.id}`} key={user.id}>
+                <User
+                    id={user.id}
+                    name={user.name}
+                    email={user.email}
+                    username={user.username}
+                />
+            </Link>
+        ));
+    }
+
+    function renderSkeletonLoading() {
+        return <h1>Loading...</h1>;
+    }
 
     return (
         <div>        
-            {users.map((user) => (
-                <Link to={`/users/${user.id}`} key={user.id}>
-                    <User
-                        id={user.id}
-                        name={user.name}
-                        email={user.email}
-                        username={user.username}
-                    />
-                </Link>
-            ))}
+            {users.length ? renderUsers() : renderSkeletonLoading()}
         </div>
     );
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,14 +1,34 @@
-import { useParams } from 'react-router-dom';
-// import React, { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Users() {
     const { id } = useParams()
+    const [user, setUser] = useState({})
+
+    async function fetchUser() {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        setUser(data)
+        console.log(data)
+    }
 
     // useEffect(() => {
     //     // fetch(`https://api.instagram.com/${username}`)
     // }, [])
     // console.log(username)
-    return <h1>{id}</h1>
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+    return (
+        <div>
+            <Link to="/"> Go back</Link>
+            <p>{ user.id }</p>
+            <p>{ user.name }</p>
+            <p>{ user.email }</p>
+            <p>{ user.username }</p>
+        </div>
+    )
 }
 
 export default Users
